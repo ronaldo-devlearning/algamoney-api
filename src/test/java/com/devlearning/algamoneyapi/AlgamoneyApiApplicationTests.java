f8874519ef7079c0a6d99a2febc653de281b5ff0
Ronaldo Rocha <ronaldo.devlearning@gmail.com>
2023-10-22 11:10:19 -0300

    Criação do projeto e configurações básicas do database

---
+++ src/test/java/com/devlearning/algamoneyapi/AlgamoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlearning.algamoneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgamoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
